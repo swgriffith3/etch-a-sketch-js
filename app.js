a42354d6879e3641674e7672c502dc65ef61317e
Stan Griffith <swgthree@gmail.com>
2020-11-15 11:59:25 -0700

    Created grid

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-alert('hello');
+function addElement() {
+  for (let i = 0; i < 256; i++) {
+    const div = document.createElement('div');
+    div.setAttribute('id', 'div-box');
+    grid.appendChild(div);
+  }
+}
+addElement();
