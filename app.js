3af57ad6dbff9b66211e089144759bad758a0ec2
Stan Griffith <swgthree@gmail.com>
2020-11-15 14:06:08 -0700

    update grid

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-function addElement() {
-  for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.setAttribute('id', 'div-box');
-    grid.appendChild(div);
-  }
+for (let i = 0; i < 256; i++) {
+  const div = document.createElement('div');
+  div.setAttribute('id', 'div-box');
+  div.setAttribute('class', 'divWhite divAqua');
+  grid.appendChild(div);
 }
-addElement();
