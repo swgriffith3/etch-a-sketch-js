1fcd53b48f4de466533c4189b44a509e5a431921
Stan Griffith <swgthree@gmail.com>
2020-11-22 12:00:47 -0700

    update all

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
-let gridSize = parseInt(prompt('Enter grid size upto 1000', 16));
+let gridSize;
+let gridNum;
 
-function createGrid() {
-  let gridNum = gridSize * gridSize;
+sizePrompt();
+
+function sizePrompt() {
+  gridSize = parseInt(prompt('Enter number of rows & colums upto 100', 16));
+  if (gridSize < 1 || gridSize > 100) {
+    gridSize = parseInt(prompt('Please enter a size between 1 and 100'));
+  } else {
+    gridSize = gridSize;
+  }
+}
+
+const grid = document.getElementById('grid');
+
+function createGrid(rows, cols) {
+  gridNum = gridSize * gridSize;
+  grid.style.setProperty('--grid-rows', rows);
+  grid.style.setProperty('--grid-cols', cols);
+
   for (let i = 0; i < gridNum; i++) {
-    const div = document.createElement('div');
+    let div = document.createElement('div');
     div.setAttribute('id', 'div-box');
-    div.setAttribute('class', 'divWhite');
+    div.setAttribute('class', 'div-white');
     grid.appendChild(div);
   }
 }
-createGrid();
+
+createGrid(gridSize, gridSize);
 
 let hover = document.getElementById('grid');
-
 hover.addEventListener('mouseover', function (e) {
   e.target.style.background = 'aqua';
 });
 
-function reset() {
-  let gridClear = grid.querySelectorAll('.divWhite');
+function resetColor() {
+  let gridClear = grid.querySelectorAll('.div-white');
   gridClear.forEach((gridClear) => (gridClear.style.backgroundColor = 'white'));
 }
